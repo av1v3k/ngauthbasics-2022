84ec16028982b5cf0809858aee32c062885b83f7
vanan07 <Vivek.Ananth@albertsons.com>
2022-07-16 20:34:15 +0530

    register comp skeleton

---
+++ frontend/src/app/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  registerUserData = { email: '', password: '' };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  registerUser() {
+    console.log(this.registerUserData);
+  }
+
+}
